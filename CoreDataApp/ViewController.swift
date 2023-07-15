b586e376dea7508bd80a62e0f21242c71e1a3ea3
Murat Ceyhun KORPEOGLU <muratceyhunkorpeoglu@gmail.com>
2023-07-15 18:50:36 +0300

    Stylizing UI and building list with tableViewController.

--- CoreDataApp/ViewController.swift
+++ CoreDataApp/ViewController.swift
@@ -7,13 +7,70 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UITableViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        navigationItem.title = "Companies"
+//        tableView.tableFooterView = UIView()
+        tableView.backgroundColor = .darkBlue
+        tableView.separatorStyle = .singleLine
+        tableView.separatorColor = .white
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "plus"), style: .plain, target: self, action: #selector(handleAddCompany))
+        setupNavStyle()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellID")
+    }
+    
+    @objc func handleAddCompany() {
+        print("GGGGGG")
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView()
+        view.backgroundColor = .lightBlue
+        return view
     }
 
-
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 60
+    }
+    
+    func setupNavStyle() {
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.backgroundColor = .lightRed
+        
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellID", for: indexPath)
+        cell.backgroundColor = .tealColor
+        cell.textLabel?.text = "Cell | \(indexPath.item)"
+        cell.textLabel?.textColor = .white
+        cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        return cell
+    }
+    
 }
+
+
+
+
+
+
 
