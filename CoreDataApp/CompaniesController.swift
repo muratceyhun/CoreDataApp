8d8f6a267af54588d1f31ee15b6bc8552242926d
Murat Ceyhun KORPEOGLU <muratceyhunkorpeoglu@gmail.com>
2023-07-30 19:48:51 +0300

    Nested parent child context relationship

--- CoreDataApp/CompaniesController.swift
+++ CoreDataApp/CompaniesController.swift
@@ -80,13 +80,60 @@ class CompaniesController: UITableViewController {
             }
         }
     }
+    
+    @objc func doNestedUpdates() {
+        
+        DispatchQueue.global(qos: .background).async {
+            
+            // we will first construct our custom MOC(managed object context)
+            
+            let privateContext = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+            privateContext.parent = CoreDataManager.shared.persistentContainer.viewContext
+            
+            //execute updates on a private context
+            
+            let request: NSFetchRequest = Company.fetchRequest()
+            request.fetchLimit = 1
+            do {
+                let companies = try privateContext.fetch(request)
+                companies.forEach { company in
+                    print(company.name ?? "")
+                    company.name = "D:\(company.name ?? "")"
+                }
+                do {
+                    try privateContext.save()
+                    
+                    DispatchQueue.main.async {
+                        
+                        do {
+                            let context = CoreDataManager.shared.persistentContainer.viewContext
+                            if context.hasChanges {
+                                try context.save()
+                            }
+                            self.tableView.reloadData()
+                        } catch let err {
+                            print("Error", err)
+                        }
+                    }
+
+                } catch let err {
+                    print("ERROR:", err)
+                }
+            } catch let err {
+                print("ERROR", err)
+            }
+            
+        }
+        
+         
+    }
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.leftBarButtonItems = [
             UIBarButtonItem(title: "Reset", style: .plain, target: self, action: #selector(handleReset)),
-            UIBarButtonItem(title: "Do Updates", style: .plain, target: self, action: #selector(doUpdates))
+            UIBarButtonItem(title: "Do Nested Updates", style: .plain, target: self, action: #selector(doNestedUpdates))
         ]
 //        editResetbutton()
         self.companies = CoreDataManager.shared.fetchCompanies()
